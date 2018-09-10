7f56630ed5511fc3557fcb706215ddd006c2a98b
congjeon <wjs982247@gmail.com>
2018-09-10 22:08:16 +0900

    assets

---
+++ Assets/Scripts/Movements/Select_Movement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select_Movement : MonoBehaviour
+{
+    public Animator anim;
+
+    public void IsTouched()
+    {
+        Debug.Log("touch!!!!!!!!");
+        anim.SetBool("isClicked", true);
+    }
+}
